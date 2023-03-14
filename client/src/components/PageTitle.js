716e295b3cb05024d780710ab12ff94bf1339035
eshu1607 <93140364+eshu1607@users.noreply.github.com>
2023-03-14 21:48:08 +0530

    done

---
+++ client/src/components/PageTitle.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function PageTitle({title}) {
+  return (
+    <h1 className="text-xl uppercase text-primary">
+        {title}
+    </h1>
+  )
+}
+
+export default PageTitle;
